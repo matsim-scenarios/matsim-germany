45b06e9e077cca6f94c9d7351815cea3d852a235
Chengqi Lu <43133404+luchengqi7@users.noreply.github.com>
2025-08-01 16:06:27 +0200

    Update GenerateFreightTripTest.java

--- src/test/java/org/matsim/prepare/longDistanceFreightGER/GenerateFreightTripTest.java
+++ src/test/java/org/matsim/prepare/longDistanceFreightGER/GenerateFreightTripTest.java
@@ -5,17 +5,8 @@ import org.apache.logging.log4j.Logger;
 import org.geotools.util.factory.Hints;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
-import org.matsim.api.core.v01.network.Network;
-import org.matsim.api.core.v01.population.Person;
-import org.matsim.api.core.v01.population.Population;
-import org.matsim.api.core.v01.population.PopulationWriter;
-import org.matsim.core.config.ConfigUtils;
-import org.matsim.core.network.NetworkUtils;
 import org.matsim.core.population.PopulationUtils;
 import org.matsim.testcases.MatsimTestUtils;
-
-import java.io.IOException;
-import java.util.List;
 
 import static org.matsim.prepare.longDistanceFreightGER.GenerateFreightPlans.createOutput_tripOD_relations;
 
@@ -31,45 +22,45 @@ public class GenerateFreightTripTest {
 	public final void test() {
 		String [] args = {
 			"--output", utils.getOutputDirectory(),
-			"--sample", "1"
-				, "--land-use-filter"
+			"--sample", "0.001",
+			"--land-use-filter"
 		};
 
 		Hints.putSystemDefault(Hints.FORCE_LONGITUDE_FIRST_AXIS_ORDER, Boolean.TRUE );
 
 		GenerateFreightPlans.main( args );
-		PopulationUtils.comparePopulations(utils.getInputDirectory() + "/german_freight.0.001pct.plans.xml.gz", utils.getOutputDirectory() + "/german_freight.0.001pct.plans.xml.gz");
+		PopulationUtils.comparePopulations(utils.getInputDirectory() + "german_freight.0.001pct.plans.xml.gz", utils.getOutputDirectory() + "german_freight.0.001pct.plans.xml.gz");
 	}
 
-	@Test
-	public final void test2() throws IOException {
-		Network network = NetworkUtils.readNetwork("https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/de/german-wide-freight/v2/germany-europe-network.xml.gz");
-		FreightAgentGenerator freightAgentGenerator = new FreightAgentGenerator(network,
-			"https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/de/german-wide-freight/raw-data/shp/NUTS_RG_20M_2016_4326.shp/NUTS_RG_20M_2016_4326.shp",
-			null, 13, 260, 0.001 / 100);
-		log.info("Freight agent generator successfully created!");
-
-		log.info("Reading trip relations...");
-		List<TripRelation> tripRelations = TripRelation.readTripRelations("https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/de/german-wide-freight/raw-data/ketten-2010.csv" );
-		log.info("Trip relations successfully loaded. There are {} trip relations", tripRelations.size());
-
-		log.info("Start generating population...");
-		Population outputPopulation = PopulationUtils.createPopulation(ConfigUtils.createConfig());
-		for (int i = 0; i < tripRelations.size(); i++) {
-			List<Person> persons = freightAgentGenerator.generateFreightAgents(tripRelations.get(i), Integer.toString(i));
-			for (Person person : persons) {
-				outputPopulation.addPerson(person);
-			}
-		}
-
-		String outputPlansPath = utils.getOutputDirectory() + "/german_freight." + 0.001 + "pct.plans.xml.gz";
-		PopulationWriter populationWriter = new PopulationWriter(outputPopulation);
-		populationWriter.write(outputPlansPath);
-
-		// Write down tsv file for visualisation and analysis
-		String freightTripTsvPath = utils.getOutputDirectory() + "/freight_trips_data.tsv";
-		createOutput_tripOD_relations(freightTripTsvPath, outputPopulation);
-	}
+//	@Test
+//	public final void test2() throws IOException {
+//		Network network = NetworkUtils.readNetwork("https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/de/german-wide-freight/v2/germany-europe-network.xml.gz");
+//		FreightAgentGenerator freightAgentGenerator = new FreightAgentGenerator(network,
+//			"https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/de/german-wide-freight/raw-data/shp/NUTS_RG_20M_2016_4326.shp/NUTS_RG_20M_2016_4326.shp",
+//			null, 13, 260, 0.001 / 100);
+//		log.info("Freight agent generator successfully created!");
+//
+//		log.info("Reading trip relations...");
+//		List<TripRelation> tripRelations = TripRelation.readTripRelations("https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/de/german-wide-freight/raw-data/ketten-2010.csv" );
+//		log.info("Trip relations successfully loaded. There are {} trip relations", tripRelations.size());
+//
+//		log.info("Start generating population...");
+//		Population outputPopulation = PopulationUtils.createPopulation(ConfigUtils.createConfig());
+//		for (int i = 0; i < tripRelations.size(); i++) {
+//			List<Person> persons = freightAgentGenerator.generateFreightAgents(tripRelations.get(i), Integer.toString(i));
+//			for (Person person : persons) {
+//				outputPopulation.addPerson(person);
+//			}
+//		}
+//
+//		String outputPlansPath = utils.getOutputDirectory() + "/german_freight." + 0.001 + "pct.plans.xml.gz";
+//		PopulationWriter populationWriter = new PopulationWriter(outputPopulation);
+//		populationWriter.write(outputPlansPath);
+//
+//		// Write down tsv file for visualisation and analysis
+//		String freightTripTsvPath = utils.getOutputDirectory() + "/freight_trips_data.tsv";
+//		createOutput_tripOD_relations(freightTripTsvPath, outputPopulation);
+//	}
 
 
 
