f0b6f136d7d55323d68cd216b5222ae48412fe53
vsp-gleich <leich@vsp.tu-berlin.de>
2026-01-06 18:45:24 +0100

    add assertion to test

--- src/test/java/org/matsim/prepare/longDistanceFreightGER/GenerateFreightTripTest.java
+++ src/test/java/org/matsim/prepare/longDistanceFreightGER/GenerateFreightTripTest.java
@@ -1,12 +1,16 @@
 package org.matsim.prepare.longDistanceFreightGER;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 import org.matsim.core.population.PopulationUtils;
+import org.matsim.core.population.routes.PopulationComparison;
 import org.matsim.testcases.MatsimTestUtils;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
+
+import static org.matsim.core.population.routes.PopulationComparison.Result.equal;
 
 public class GenerateFreightTripTest {
 
@@ -27,6 +31,8 @@ public class GenerateFreightTripTest {
 		//		GenerateFreightPlans.main( null );
 		// do this with null argument for the time being to really make sure that it uses the same setup as that main method (since that runs and this one here fails). kai, aug'25
 
-		PopulationUtils.comparePopulations(utils.getClassInputDirectory() + "german_freight.0.001pct.plans.xml.gz", utils.getOutputDirectory() + "german_freight.0.001pct.plans.xml.gz");
+		PopulationComparison.Result comparison = PopulationUtils.comparePopulations(utils.getClassInputDirectory() + "german_freight.0.001pct.plans.xml.gz", utils.getOutputDirectory() + "german_freight.0.001pct.plans.xml.gz");
+
+		Assertions.assertEquals(equal, comparison);
 	}
 }
