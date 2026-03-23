bffe6e619f5952293e745144644fcd5822876f76
apabis <aleksanderpabis@gmail.com>
2026-03-23 11:33:35 +0100

    Added TransitSchedules (without vehicles)

--- src/main/java/org/matsim/plane/CreatePlaneSchedule.java
+++ src/main/java/org/matsim/plane/CreatePlaneSchedule.java
@@ -6,14 +6,22 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.matsim.api.core.v01.Coord;
 import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.api.core.v01.network.Link;
 import org.matsim.api.core.v01.network.Network;
 import org.matsim.api.core.v01.network.Node;
 import org.matsim.application.MATSimAppCommand;
 import org.matsim.application.options.CrsOptions;
+import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.network.NetworkUtils;
+import org.matsim.core.population.routes.NetworkRoute;
+import org.matsim.core.population.routes.RouteUtils;
+import org.matsim.core.scenario.ScenarioUtils;
 import org.matsim.core.utils.collections.Tuple;
 import org.matsim.core.utils.geometry.CoordUtils;
 import org.matsim.core.utils.geometry.CoordinateTransformation;
+import org.matsim.pt.transitSchedule.TransitScheduleFactoryImpl;
+import org.matsim.pt.transitSchedule.api.*;
 import picocli.CommandLine;
 
 import java.io.IOException;
@@ -21,6 +29,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.*;
 
 /**
@@ -34,6 +43,8 @@ import java.util.*;
  * using the {@link AirportNetworkFuser}.
  */
 public class CreatePlaneSchedule implements MATSimAppCommand {
+	// TODO Vehicle Types are a problem, no free data for ICAO codes available!
+
 	private static final Logger log = LogManager.getLogger(CreatePlaneSchedule.class);
 
 	@CommandLine.Option(names = "--adrr-flight-data", description = "Path to the flights file of the EUROCONTROL ADRR dataset", required = true)
@@ -72,10 +83,12 @@ public class CreatePlaneSchedule implements MATSimAppCommand {
 		try (var reader = Files.newBufferedReader(Path.of(flightPath)); var parser = CSVParser.parse(reader, format)) {
 			for(var record : parser){
 				flightInformationList.add( new FlightInformation(
+					record.get("ECTRL ID"),
 					record.get("ADEP"),
 					record.get("ADES"),
 					LocalDateTime.parse(record.get("FILED OFF BLOCK TIME"), formatter),
-					LocalDateTime.parse(record.get("FILED ARRIVAL TIME"), formatter)
+					LocalDateTime.parse(record.get("FILED ARRIVAL TIME"), formatter),
+					Double.parseDouble(record.get("Actual Distance Flown (nm)"))*1852
 				));
 
 				String lonADEP = record.get("ADEP Longitude");
@@ -99,34 +112,94 @@ public class CreatePlaneSchedule implements MATSimAppCommand {
 			throw new RuntimeException(e);
 		}
 
+		// Prepare Scenario
+		Scenario planeScenario = ScenarioUtils.createScenario(ConfigUtils.createConfig());
+		TransitScheduleFactoryImpl scheduleFactory = new TransitScheduleFactoryImpl();
+
 		// Create the plane-network and add airports into it (without connections)
-		Network planeNetwork = NetworkUtils.createNetwork();
 		for(var airport : airportSet){
-			addAirportToNetwork(planeNetwork, airport);
+			addAirportToNetwork(planeScenario.getNetwork(), airport);
+			planeScenario.getTransitSchedule().addStopFacility(scheduleFactory.createTransitStopFacility(
+				Id.create(airport.getFirst(), TransitStopFacility.class),
+				CoordUtils.plus(airport.getSecond(), new Coord(2000, 500)),
+				false
+			));
 		}
 
 		// Create schedule with given flightInformation
 		// TODO
+		for(var flightInformation : flightInformationList){
+			String id = flightInformation.ADEP + "_" + flightInformation.ADES + "_" + flightInformation.id;
+			Id<Link> linkId = Id.createLinkId(id);
+			Id<TransitLine> lineId = Id.create(id, TransitLine.class);
+			Id<TransitRoute> routeId = Id.create(id, TransitRoute.class);
+
+			// Add the link for this flight
+			double time = ChronoUnit.SECONDS.between(flightInformation.filedOffBlockTime, flightInformation.filedArrivalTime);
+
+			planeScenario.getNetwork().addLink(NetworkUtils.createLink(
+				linkId,
+				planeScenario.getNetwork().getNodes().get(Id.createNodeId(flightInformation.ADEP + "runwayOutbound")),
+				planeScenario.getNetwork().getNodes().get(Id.createNodeId(flightInformation.ADES + "runwayInbound")),
+				planeScenario.getNetwork(),
+				flightInformation.distance,
+				flightInformation.distance / time,
+				3600,
+				1
+			));
+
+			// Add the transitLine
+
+			planeScenario.getTransitSchedule().addTransitLine(scheduleFactory.createTransitLine(lineId));
+
+			List<Id<Link>> links = List.of(
+				Id.createLinkId(flightInformation.ADEP + "taxiOutbound"),
+				Id.createLinkId(flightInformation.ADEP + "runwayOutbound"),
+				linkId,
+				Id.createLinkId(flightInformation.ADES + "runwayInbound"),
+				Id.createLinkId(flightInformation.ADES + "taxiInbound")
+			);
+
+			NetworkRoute route = RouteUtils.createLinkNetworkRouteImpl(
+				Id.createLinkId(flightInformation.ADEP),
+				links,
+				Id.createLinkId(flightInformation.ADES)
+			);
+
+			List<TransitRouteStop> stops = List.of(
+				scheduleFactory.createTransitRouteStop(planeScenario.getTransitSchedule().getFacilities().get(Id.create(flightInformation.ADEP, TransitStopFacility.class)), 0, 0),
+				scheduleFactory.createTransitRouteStop(planeScenario.getTransitSchedule().getFacilities().get(Id.create(flightInformation.ADEP, TransitStopFacility.class)), time, time)
+			);
+
+			planeScenario.getTransitSchedule().getTransitLines().get(lineId).addRoute(scheduleFactory.createTransitRoute(routeId, route, stops, "pt"));
+		}
 
 		// Print output files
-		NetworkUtils.writeNetwork(planeNetwork, outputNetwork);
-		// TODO schedule
+		NetworkUtils.writeNetwork(planeScenario.getNetwork(), outputNetwork);
+		new TransitScheduleWriter(planeScenario.getTransitSchedule()).writeFile(outputSchedule);
 
 		return (Integer) 0;
 	}
 
-	// TODO Complete airport
 	private void addAirportToNetwork(Network planeNetwork, Tuple<String, Coord> airport){
 		List<Node> airportNodes = new ArrayList<>();
-		airportNodes.add(NetworkUtils.createNode(Id.createNodeId(airport.getFirst() + "_n0"), airport.getSecond()));
-		airportNodes.add(NetworkUtils.createNode(Id.createNodeId(airport.getFirst() + "_n1"), CoordUtils.plus(airport.getSecond(), new Coord(1000, 0))));
+		airportNodes.add(NetworkUtils.createNode(Id.createNodeId(airport.getFirst() + "runwayInbound"), airport.getSecond()));
+		airportNodes.add(NetworkUtils.createNode(Id.createNodeId(airport.getFirst() + "taxiInbound"), CoordUtils.plus(airport.getSecond(), new Coord(1500, 0))));
+		airportNodes.add(NetworkUtils.createNode(Id.createNodeId(airport.getFirst() ), CoordUtils.plus(airport.getSecond(), new Coord(2000, 500))));
+		airportNodes.add(NetworkUtils.createNode(Id.createNodeId(airport.getFirst() + "apron"), CoordUtils.plus(airport.getSecond(), new Coord(2000, 1000))));
+		airportNodes.add(NetworkUtils.createNode(Id.createNodeId(airport.getFirst() + "taxiOutbound"), CoordUtils.plus(airport.getSecond(), new Coord(1500, 1500))));
+		airportNodes.add(NetworkUtils.createNode(Id.createNodeId(airport.getFirst() + "runwayOutbound"), CoordUtils.plus(airport.getSecond(), new Coord(0, 1500))));
 
 		for(var node : airportNodes){
 			planeNetwork.addNode(node);
 		}
 
-		planeNetwork.addLink(NetworkUtils.createLink(Id.createLinkId(airport.getFirst() + "_runway"), airportNodes.get(0), airportNodes.get(1), planeNetwork, 1000, 300, 1000, 1));
+		planeNetwork.addLink(NetworkUtils.createLink(Id.createLinkId(airport.getFirst() + "runwayInbound"), airportNodes.get(0), airportNodes.get(1), planeNetwork, 1500, 1500, 3600, 1));
+		planeNetwork.addLink(NetworkUtils.createLink(Id.createLinkId(airport.getFirst() + "taxiInbound"), airportNodes.get(1), airportNodes.get(2), planeNetwork, 500, 20/3.6, 3600, 1));
+		planeNetwork.addLink(NetworkUtils.createLink(Id.createLinkId(airport.getFirst()), airportNodes.get(2), airportNodes.get(3), planeNetwork, 500, 20/3.6, 3600, 1));
+		planeNetwork.addLink(NetworkUtils.createLink(Id.createLinkId(airport.getFirst() + "taxiOutbound"), airportNodes.get(3), airportNodes.get(4), planeNetwork, 500, 20/3.6, 3600, 1));
+		planeNetwork.addLink(NetworkUtils.createLink(Id.createLinkId(airport.getFirst() + "runwayOutbound"), airportNodes.get(4), airportNodes.get(5), planeNetwork, 1500, 1500, 3600, 1));
 	}
 
-	record FlightInformation(String ADEP, String ADES, LocalDateTime filedOffBlockTime, LocalDateTime filedArrivalTime){}
+	record FlightInformation(String id, String ADEP, String ADES, LocalDateTime filedOffBlockTime, LocalDateTime filedArrivalTime, double distance){}
 }
